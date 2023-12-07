0899a611aa56a31fc0ae0c9cfdb970c777f44950
TPlatts04 <thomasplatts2004@gmail.com>
2023-12-07 19:11:59 +0000

    Setup notes of JS calculator

--- calculator.js
+++ calculator.js
@@ -1,4 +1,7 @@
-
+// 1. Take user input
+// 2. Cycle through operation choices and let user select
+// 3. Keep asking user until they want operation to be completed
+// 4. Calculate and output
 
 const prompt = require("prompt-sync")();
 
